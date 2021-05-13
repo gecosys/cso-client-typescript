3b2ecfaf813dd3eecb500d822c64385dd0a5e7df
Nguyễn Văn Hoà <1712448@student.hcmus.edu.vn>
2021-05-13 11:15:29 +0700

    fix: input VerifyRSASign

--- src/utils/utils_rsa.ts
+++ src/utils/utils_rsa.ts
@@ -1,10 +1,10 @@
 import * as Crypto from "crypto";
 export function VerifyRSASign(
+  publicKey: string,
   data: Uint8Array,
-  publicKey: Uint8Array,
   sign: Uint8Array
 ) {
   let verify = Crypto.createVerify("RSA-SHA256");
   verify.update(new TextDecoder("utf-8").decode(data), "utf-8");
-  return verify.verify(new TextDecoder("utf-8").decode(publicKey), sign);
+  return verify.verify(publicKey, sign);
 }
